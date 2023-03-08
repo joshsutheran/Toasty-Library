26c5b4d69374936be88306d512fd0e2505eeb2ef
Josh Sutheran <josh.sutheran@gmail.com>
2023-03-08 23:29:06 +0000

    Update toast.js

--- toast.js
+++ toast.js
@@ -38,7 +38,7 @@ class Toast {
     if (!value || value === 'success') {
       this.#toastEl.classList.add('successful__toast')
       this.#toastIconEl = document.createElement('div')
-      // this.#toastIconEl.classList.add('toast__iconEl')
+      this.#toastIconEl.classList.add('toast__iconEl')
       this.#toastIconEl.classList.add('successful_toast_icon')
       this.#iconName = 'check-circle'
     } else if (!value || value === 'information') {
